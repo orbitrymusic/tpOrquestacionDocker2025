749fa6ee5f38a43d7309b9e2711fa2add2c7008c
Adrian853tp <burdilesricardo407@gmail.com>
2025-10-15 19:37:51 -0300

    feature-AB-controllers and services

---
+++ src/services/userService.js
@@ -0,0 +1,55 @@
+import Usuario from '../models/Usuario.js';
+import bcrypt from 'bcrypt';
+
+class UserService {
+  constructor() {
+    this.model = Usuario;
+  }
+
+  async register(data) {
+    console.log("Iniciando la función de registro...");
+    try {
+      const { nombre, email, password } = data;
+      const newUser = new this.model({
+        nombre,
+        email,
+        password: password,
+      });
+
+      const savedUser = await newUser.save(); // <-- Aquí esperamos que se guarde.
+      return savedUser; // <-- Devolvemos el usuario guardado.
+
+    } catch (error) {
+      console.error("Error al guardar el usuario en la base de datos:", error);
+      throw error; // <-- Lanzamos el error para que el controlador lo atrape.
+    }
+  }
+
+  async login(email, password) {
+    const user = await this.model.findOne({ email }).select('+password');
+    if (!user) {
+      return null;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch ? user : null;
+  }
+  async getAll() {
+    return this.model.find().select('-password');
+  }
+
+  //* modificar nombre usuario 
+  async update(userId, updateData) {
+    try {
+      const user = await Usuario.findByIdAndUpdate(userId, updateData, {new: true});
+      if(!user){
+        return null;
+      }
+      return user;
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+      throw error;
+    }
+  }
+}
+
+export default new UserService();
